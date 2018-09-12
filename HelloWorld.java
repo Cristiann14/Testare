6f19194ca1595488e1998043a4761adbf4ed2a20
Cristian Prepelita <cristianp@lt-air-buc-004.ny.rga.com>
2018-09-12 11:18:13 +0300

    mesaj3

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,10 @@
 public class HelloWorld
 {
   public static void main(String args[])
-  
-       System.out.println("Test");
-
+  {
+      for (int i=0;i<=5;i++)
+      {
+        System.out.println("Test"+i);
+      }   
+  }
 }
